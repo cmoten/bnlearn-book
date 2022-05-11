fa595eecb4816ed5e6665a442308c22c10d865e9
cmoten <cmoten@gmail.com>
2022-05-11 04:59:12 -0400

    updated toy model

--- toy-example.R
+++ toy-example.R
@@ -17,21 +17,32 @@ rank_order_centroid <- function(n){
 }
 #Network-----
 #Nodes
-dag <- empty.graph(nodes = LETTERS[1:3])
+dag <- empty.graph(nodes = LETTERS[1:4])
 
 #Arcs
-dag <- set.arc(dag, from = "A", to = "B")
+dag <- set.arc(dag, from = "A", to = "C")
+dag <- set.arc(dag, from = "A", to = "D")
 dag <- set.arc(dag, from = "C", to = "B")
+dag <- set.arc(dag, from = "D", to = "B")
 
 
 #Set Levels
-A.lv <- B.lv <- C.lv <- c("SI", "MI", "LI", "NI")
+A.lv <- B.lv <- C.lv <- D.lv <- c("SI", "MI", "LI", "NI")
 
 #Set Probabilities
-b_weights <- rank_order_centroid(4)
+#b_weights <- rank_order_centroid(4)
 sample_probs <- c(0.3, 0.4, 0.2, 0.1)
 A.prob <- array(sample_probs, dim = 4, dimnames = list(A = A.lv))
-C.prob <- array(sample(sample_probs), dim = 4, dimnames = list(C = B.lv))
+C.prob <- array(c(0.9, 0.067, 0.0165, 0.0165,
+                  0.0425, 0.9, 0.0425, 0.015,
+                  0.015, 0.0425, 0.9, 0.0425,
+                  0.0165, 0.0165, 0.067, 0.9), dim = c(4, 4),
+                dimnames = list(C = C.lv, A = A.lv))
+D.prob <- array(c(0.9, 0.067, 0.0165, 0.0165,
+                  0.0425, 0.9, 0.0425, 0.015,
+                  0.015, 0.0425, 0.9, 0.0425,
+                  0.0165, 0.0165, 0.067, 0.9), dim = c(4, 4),
+                dimnames = list(D = C.lv, A = A.lv))
 B.prob <- array(c(0.9, 0.067, 0.0165, 0.0165,
                   0.067, 0.9, 0.0165, 0.0165,
                   0.022, 0.8, 0.134, 0.044,
@@ -49,18 +60,18 @@ B.prob <- array(c(0.9, 0.067, 0.0165, 0.0165,
                   0.022, 0.044, 0.8, 0.134,
                   0.0165, 0.0165, 0.067, 0.9),
                 dim = c(4, 4, 4),
-                dimnames = list(B = B.lv, A = A.lv, C = C.lv))
+                dimnames = list(B = B.lv, C = C.lv, D = D.lv))
 
 
 
 
 #Fit Model
-toy_model_cpt <- list(B = B.prob, A = A.prob, C = C.prob)
+toy_model_cpt <- list(B = B.prob, A = A.prob, C = C.prob, D = D.prob)
 toy_model_bn <- custom.fit(dag, toy_model_cpt)
 
 #Graph Model
 graphviz.chart(toy_model_bn, type = "barprob", grid = TRUE, bar.col = "green",
-               strip.bg = "lightyellow")
+               strip.bg = "lightyellow", layout = "neato")
 
 #Analyze Evidence
 #'What is the probability distribution of Objective B given that Objective A is rates as "LI"?
@@ -71,7 +82,7 @@ querygrain(obj_b_dist, nodes = "B")$B
 #'What is the probability distribution of Objective B given that Objective A is rates as "LI",
 #'and Objective C is rated as "MI"?
 extended_junction <- compile(as.grain(toy_model_bn))
-obj_b_dist <- setEvidence(extended_junction, nodes = c("A", "C"), states = c("LI", "MI"))
+obj_b_dist <- setEvidence(extended_junction, nodes = c("A", "C", "D"), states = c("MI", "SI", "MI"))
 querygrain(obj_b_dist, nodes = "B")$B
 graphviz.chart(as.bn.fit(obj_b_dist, including.evidence = TRUE), type = "barprob", grid = TRUE, bar.col = "green",
-               strip.bg = "lightyellow")
+               strip.bg = "lightyellow", layout = "neato")
